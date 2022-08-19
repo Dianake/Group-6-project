9e2c7c53f4e127dcb32df3c03ca1be4161ae4154
iris-zaf <zafi2313@hotmail.com>
2022-08-19 22:23:43 +0300

    added some javascript-still working on the buttons

---
+++ js/aboutus.js
@@ -0,0 +1,23 @@
+function revealMessage() {
+    var text = document.getElementById("hiddenMessage");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+  function buttononClick() {
+    document.getElementById("card").style.display = "block";
+  }
+
+  function buttononClick1() {
+    document.getElementById("card1").style.display = "block";
+  }
+
+  function buttononClick2() {
+    document.getElementById("card2").style.display = "block";
+  }
+
+  function buttononClick3() {
+    document.getElementById("card3").style.display = "block";
+  }
