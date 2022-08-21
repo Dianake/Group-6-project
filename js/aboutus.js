ca090a3dbaec93eeafab6530a4c80115f1a3ab11
iris-zaf <zafi2313@hotmail.com>
2022-08-21 20:55:50 +0300

    added popular destination cards

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,23 +1,12 @@
 function revealMessage() {
     var text = document.getElementById("hiddenMessage");
-    if (text.style.display === "none") {
-      text.style.display = "block";
-    } else {
+    if (text.style.display === "block") {
       text.style.display = "none";
+    } else {
+      text.style.display = "block";
     }
   }
-  function buttononClick() {
-    document.getElementById("card").style.display = "block";
-  }
+ 
+  
 
-  function buttononClick1() {
-    document.getElementById("card1").style.display = "block";
-  }
-
-  function buttononClick2() {
-    document.getElementById("card2").style.display = "block";
-  }
-
-  function buttononClick3() {
-    document.getElementById("card3").style.display = "block";
-  }
+  
