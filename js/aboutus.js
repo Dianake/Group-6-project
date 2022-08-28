a9f80d312cf10b9edfa25da0cc8891dfea36c6a7
iris-zaf <zafi2313@hotmail.com>
2022-08-28 22:43:05 +0300

    change pictures on the aboutus

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,12 +1,19 @@
-function revealMessage() {
-    var text = document.getElementById("hiddenMessage");
-    if (text.style.display === "block") {
-      text.style.display = "none";
-    } else {
-      text.style.display = "block";
-    }
-  }
- 
-  
 
-  
+
+
+
+/* newsletter */
+function revealMessage() {
+  var text = document.getElementById("hiddenMessage");
+  if (text.style.display === "block") {
+    text.style.display = "none";
+  } else {
+    text.style.display = "block";
+  }
+}
+
+/*button to change color */
+function color(value){
+  document.getElementById("aboutus").style.background=value
+  document.getElementById("aboutus").style.backgroundSize = "cover";
+}
